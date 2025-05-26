a3cc1b82ce7d2645821bceb433f7f093a6ea883c
younes-oss <aitchattouyounes@gmail.com>
2025-05-26 12:24:36 +0200

    la création du classe JwtAuthenticationFilter

--- src/main/java/org/example/eventsapp/config/JwtAuthenticationFilter.java
+++ src/main/java/org/example/eventsapp/config/JwtAuthenticationFilter.java
@@ -5,6 +5,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,6 +15,9 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtSevice jwtSevice;
+    private UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -20,12 +26,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         final String authorizationHeader = request.getHeader("Authorization");
-        final String Jwt;
+        final String jwt;
+        final String userEmail;
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
-        Jwt = authorizationHeader.substring(7);
+        jwt = authorizationHeader.substring(7);
+        userEmail= jwtSevice.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails =  this.userDetailsService.loadUserByUsername(userEmail);
+        }
 
     }
 }
---
+++ src/main/java/org/example/eventsapp/config/JwtSevice.java
@@ -0,0 +1,74 @@
+package org.example.eventsapp.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class JwtSevice {
+
+    private static final String SECRET_KEY = "Qz8x@4Lm9$N#sK2e!D9fR8pW^X6bC1zA7LkY#Vp3UqE@tG0H";
+
+    public String extractUsername(String token) {
+
+        return extractClaim(token,Claims :: getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String GeneratToken(UserDetails userDetails) {
+        return GeneratToken(new HashMap<>(), userDetails);
+    }
+
+    public String GeneratToken( Map<String, Object> extraClaims,
+    UserDetails userDetails) {
+            return Jwts
+                    .builder()
+                    .setClaims(extraClaims)
+                    .setSubject(userDetails.getUsername())
+                    .setIssuedAt(new Date(System.currentTimeMillis()))
+                    .setExpiration(new Date(System.currentTimeMillis() +1000 *60 *24))
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                    .compact();
+
+    }
+
+    public boolean isTokenValid(String token , UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token ){
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token,Claims::getExpiration);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
